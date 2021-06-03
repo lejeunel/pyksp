7cec05467cecda1d8aafa55227c46b2da663dc25
olol85 <olol85@gmail.com>
2021-06-03 17:42:43 +0200

    readme and demo comments

--- demo.py
+++ demo.py
@@ -8,9 +8,12 @@ import matplotlib.pyplot as plt
 import os
 import glob
 
+# Define nodes by strings and impose a correspondence with integers
 nodes = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'z': 7}
 inv_nodes = {v: k for k, v in nodes.items()}
 
+# build adjacency list: each row contains two integers where the
+# first is the input node, and the second is the output node of an edge
 A = []
 A.append((nodes['a'], nodes['b']))
 A.append((nodes['b'], nodes['c']))
@@ -27,12 +30,28 @@ A.append((nodes['g'], nodes['z']))
 A.append((nodes['c'], nodes['g']))
 A = np.array(A)
 
+# define weights (lenghts) of each edge
 W = np.array([1, 1, 1, 1, 1, 3, 5, 1, 1, 2, 7, 2, 1])
 
-tracker = pyksp.PyKsp(A[:, 0], A[:, 1], W, np.unique(A).size, 0, 7)
-tracker.config(min_cost=False, verbose=True, l_max=3)
+# initialize graph object using arguments:
+# - ndarray of shape [N] where each element is the index of an input node
+# - ndarray of shape [N] where each element is the index of an output node
+# - ndarray of shape [N] where each element is the weight (length) of the corresponding edge
+# - integer: total number of nodes in the graph
+# - integer: index of source node
+# - integer: index of sink node
+graph = pyksp.PyKsp(A[:, 0], A[:, 1], W, np.unique(A).size, 0, 7)
 
-res = tracker.run()
+# Since all our edges have positive lengths, the obvious solution would be the empty set
+# We set min_cost to False to force the algorithm to find some paths
+# l_max governs the maximum number of path to find (ineffective when min_cost is True)
+# The verbose flag will print progression to stdout
+graph.config(min_cost=False, verbose=True, l_max=3)
+
+# run algorithm
+res = graph.run()
+
+#Below, we produce a plot of solutions
 
 # delete graphviz files that could exist
 files = glob.glob(os.path.join('*.gv.*'))
